3fc63cd67206b175ef587c4d60b7eac1827f4e44
vasyakov <vasyakov94@gmail.com>
2020-10-13 21:26:44 +0300

    Add background in project

--- core/src/ru/starwars/StarWars.java
+++ core/src/ru/starwars/StarWars.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import javax.swing.JFrame;
 
 public class StarWars extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("background.jpg");
 	}
 
 	@Override
@@ -21,7 +24,7 @@ public class StarWars extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,0,0,640,480);
 		batch.end();
 	}
 	
